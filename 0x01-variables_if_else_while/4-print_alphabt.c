6d66f8cddcfe4c93eca191a47562b241a24545d7
Rabiu <harkeentundewadewasiu@gmail.com>
2022-10-15 02:07:45 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all the letters except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+	
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la :a 'e' && la := 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
