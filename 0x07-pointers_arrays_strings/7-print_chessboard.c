c86f3d198d64589f15f68c477e98d514740168a7
Rabiu <harkeentundewadewasiu@gmail.com>
2022-11-01 04:53:16 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i1, i2
+	int index1, index2
 
-	for (i1 = 0; a[i1][7]; i1++)
+	for (index1 = 0; a[index1][7]; index1++)
 
 	{
-		for (i2 = 0; i2 < 8; i2++)
-			_putchar(a[i1][i2]);
+		for (index2 = 0; index2 < 8; index2++)
+			_putchar(a[index1][index2]);
 
 		_putchar('\n');
 
