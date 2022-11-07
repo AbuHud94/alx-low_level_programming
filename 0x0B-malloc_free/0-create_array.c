5681800852ca50ef70a4246a4f47fc73de10f48c
Rabiu <harkeentundewadewasiu@gmail.com>
2022-11-07 06:13:32 -0800

    task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,30 +8,31 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+char *buffer;
+unsigned int position;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
+if (size == 0)
+{
+return (NULL);
+}
 
-	/* define values with malloc */
-	buffer = (char *) malloc(size * sizeof(c));
+/* define values with malloc */
+buffer = (char *) malloc(size * sizeof(c));
 
-	if (buffer == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		position = 0;
-	while (position < size) /* while for aray */
-	{
-		*(buffer + position) = c;
-		position++;
-	}
+if (buffer == 0)
+{
+return (NULL);
+}
 
-	return (buffer);
-	}
+else
+{
+position = 0;
+while (position < size) /* while for aray */
+{
+*(buffer + position) = c;
+position++;
+}
+
+return (buffer);
+}
 }
