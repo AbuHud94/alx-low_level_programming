48fa095ca5b8341db85d8552a3ab95f5e656445a
Rabiu <harkeentundewadewasiu@gmail.com>
2022-11-08 06:22:00 -0800

    checked error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * alloc_grid - allowance a grid, make space and free space
+ * alloc_grid - allocate a grid, make space and free space
  * @width: takes in width of the grid
  * @height: height of grid
  * Return: grid with freed spaces
@@ -8,36 +8,36 @@
 
 int **alloc_grid(int width, int height)
 {
-	/*declaring variables*/
-	int **grid;
-	int i, j;
+/*declaring variables*/
+int **grid;
+int i, j;
 
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
-	grid = malloc(sizeof(int *) * height); /*malloc*/
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
-	if (grid == NULL)
-	{
-		return (NULL);
-	}
+if (grid == NULL)
+{
+return (NULL);
+}
 
-	for (i = 0; i < height; i++)
-	{
-	grid[i] = malloc(sizeof(int) * width);
-	if (grid[i] == NULL)
-	{
-	for (i = i - 1; 1 >= 0; i--)
-	{
-		free(grid[i]);
-	}
-	free(grid);
-	return (NULL);
-	}
-	}
-	for (i = 0; j < width; j++)
-		grid[i][j] = 0;
-	return (grid);
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (i = i - 1; 1 >= 0; i--)
+{
+free(grid[i]);
+}
+free(grid);
+return (NULL);
+}
+}
+for (i = 0; j < width; j++)
+grid[i][j] = 0;
+return (grid);
 }
